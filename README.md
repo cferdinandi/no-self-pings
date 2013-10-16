9d5a92a7a2821da1d262ad276a9026108fe943b3
Chris Ferdinandi <hello@gomakethings.com>
2013-10-16 17:28:01 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# No Self Pings
+A simple plugin to prevent self-pings, forked from [WPMU](http://wpmu.org/daily-tip-3-ways-to-remove-wordpress-self-pings/).
+
+
+## How It Works
+To get started with No Self Pings, [view the online tutorial](http://cferdinandi.github.com/no-self-pings/).
+
+
+## Changelog
+* v1.0 (October 16, 2013)
+  * Initial release.
+
+
+## License
+No Self Pings is free to use under the [MIT License](http://gomakethings.com/mit/).
---
+++ no-self-pings.php
@@ -0,0 +1,28 @@
+<?php
+
+/* ======================================================================
+
+	Plugin Name: No Self Pings
+	Plugin URI: http://cferdinandi.github.io/no-self-pings/
+	Description: Prevents internal links from showing up as pings/trackbacks in your comments.
+	Version: 1.0
+	Author: Chris Ferdinandi
+	Author URI: http://gomakethings.com
+	License: MIT
+
+	Forked from WPMU.
+	http://wpmu.org/daily-tip-3-ways-to-remove-wordpress-self-pings/
+
+ * ====================================================================== */
+
+function nsp_no_self_ping( &$links ) {
+	$home = get_option( 'home' );
+	foreach ( $links as $l => $link ) {
+		if ( 0 === strpos( $link, $home ) ) {
+			unset($links[$l]);
+		}
+	}
+}
+add_action( 'pre_ping', 'nsp_no_self_ping' );
+
+?>
